5e8992b431ea4ad5ac3e5931da248d51762d2930
Gerley Machado <gerleymachado@gmail.com>
2025-09-02 20:32:01 -0300

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
-import "github.com/Gerley/gopportunities/router"
+import (
+	"github.com/Gerley/gopportunities/config"
+	"github.com/Gerley/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLooger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	// Initialize Router
 	router.Initialize()
 }
+
+// https://www.youtube.com/watch?v=wyEYpX5U4Vg
