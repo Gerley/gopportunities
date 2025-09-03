e166039c270ce0af0c90b8faccea53cdcb548aa2
Gerley Machado <gerleymachado@gmail.com>
2025-09-02 22:13:25 -0300

    feat: add createOpening logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	"github.com/Gerley/gopportunities/config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLooger("handler")
+	db = config.GetSQlite()
+}
